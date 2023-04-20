fd56bb36e741e2c96306e79038caaa374d0000ab
John Ixcoy <jixcoy316@gmail.com>
2023-04-20 12:49:09 -0700

    finished lab 3

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    
+    /*Declare a new private float speed variable
+If using physics, declare a new Rigidbody playerRb variable for it and initialize it in Start()
+If using arrow keys, declare new verticalInput and/or horizontalInput variables 
+If basing your movement off a key press, create the if-statement to test for the KeyCode
+Use either the Translate method or AddForce method (if using physics) to move your character*/
+
+    public float speed;
+    public float turnSpeed;
+    public float xMoveInput;
+    private float yMoveInput;
+
+    private Rigidbody rb;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody>();
     }
-
+    
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        xMoveInput = Input.GetAxisRaw("Horizontal");
+        yMoveInput = Input.GetAxisRaw("Vertical");
+
+        transform.Translate(Vector3.forward * (Time.deltaTime * speed * yMoveInput));
+        if (xMoveInput != 0)
+        {
+            transform.Rotate(Vector3.up, (Time.deltaTime * turnSpeed * xMoveInput));
+
+        }
     }
 }
